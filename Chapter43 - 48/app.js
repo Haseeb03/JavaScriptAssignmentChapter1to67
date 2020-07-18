bbd91e2f151c463d25bd438ac37bcf984668e6d2
Haseeb03 <m.haseeb5678@gmail.com>
2020-07-18 15:56:24 +0500

    Sayalni JavaScript Assignment from Chapter 1 to chapter67

--- Chapter43 - 48/app.js
+++ Chapter43 - 48/app.js
@@ -16,13 +16,34 @@ function Link() {
     alert("Thank you for clicking");
 }
 
-document.write("<h3>Question2</h3>");
 
+document.write("<h3>Question2</h3>");
+document.write("<h4> Mobile Phone lookup </h4>");
+document.write("<div class='moblie'> <img src='images/m1.png' onclick='message()'> <p> Tecno Spark 4 </p> </div>");
+document.write("<div class='moblie'> <img src='images/m2.png' onclick='message()'> <p> Nokia 7.2 </p> </div>");
+document.write("<div class='moblie'> <img src='images/m3.png' onclick='message()'> <p> Samsumg Galaxy Note 10 </p> </div>");
+document.write("<div class='moblie'> <img src='images/m4.png' onclick='message()'> <p> Huawei Honor 9N </p> </div>");
+function message(){
+    alert('Thanks for purchasing a phone from us');
+}
 
 document.write("<h3>Question3</h3>");
+document.write("<table id='myTable'>");
+document.write("<tr><th> index  </th><th> name </th><th> class </th> </tr>");
+document.write("<tr><td>0</td><td>Jhone</td><td> 10 </td><td><input type = 'button' value = 'Delete' onclick = 'deleteRow()' /></td></tr>");
+document.write("<tr><td>1</td><td> Doe </td><td>  9 </td><td><input type = 'button' value = 'Delete' onclick = 'deleteRow()' /></td></tr>");
+document.write("<tr><td>2</td><td>Mark </td><td> 10 </td><td><input type = 'button' value = 'Delete' onclick = 'deleteRow()' /></td></tr>");
+document.write("<tr><td>3</td><td>James</td><td>  8 </td><td><input type = 'button' value = 'Delete' onclick = 'deleteRow()' /></td></tr>");
+document.write("</table>");
+function deleteRow() {
+	var tabledata = event.target.parentNode;	 
+	var tablerow = tabledata.parentNode; 	
+	tablerow.parentNode.removeChild(tablerow);
+}
 
 
 document.write("<h3>Question4</h3>");
+
 
 
 document.write("<h3>Question5</h3>");
