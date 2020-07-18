5cd65cb7b97f79ac45b8f34020baf78a30f87120
Haseeb03 <m.haseeb5678@gmail.com>
2020-07-18 13:25:31 +0500

    Javasript Assignment from chapter1 to chapter38

---
+++ Chapter49 - 52/app.js
@@ -0,0 +1,19 @@
+document.write("<h3>Question1</h3>");
+document.write("<p>Calculate Power</p>");
+document.write("<input id='base' placeholder='Base value'>");
+document.write("<input id='power' placeholder='Exponent value'>");
+document.write("<p id='result'></p>");
+document.write("<button type='submit' onclick='CalculatePower()'>Calculate</button>");
+function CalculatePower() {
+    var b = document.getElementById("base").value;
+    var p = document.getElementById("power").value;
+    var result;
+    result = Power(b, p);
+    document.getElementById("result").innerHTML = b + " power " +p+ " = " + result;
+}
+
+
+document.write("<h3>Question2</h3>");
+
+
+document.write("<h3>Question3</h3>");
