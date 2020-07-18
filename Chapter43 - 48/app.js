3310b1fe62270d4a595441fbc6d30d6c3bb4e24d
Haseeb03 <m.haseeb5678@gmail.com>
2020-07-18 13:40:30 +0500

    Javasript Assignment from chapter1 to chapter67

--- Chapter43 - 48/app.js
+++ Chapter43 - 48/app.js
@@ -1,17 +1,20 @@
 document.write("<h3>Question1</h3>");
-document.write("<p>Calculate Power</p>");
-document.write("<input id='base' placeholder='Base value'>");
-document.write("<input id='power' placeholder='Exponent value'>");
-document.write("<p id='result'></p>");
-document.write("<button type='submit' onclick='CalculatePower()'>Calculate</button>");
-function CalculatePower() {
-    var b = document.getElementById("base").value;
-    var p = document.getElementById("power").value;
-    var result;
-    result = Power(b, p);
-    document.getElementById("result").innerHTML = b + " power " +p+ " = " + result;
+// document.write("<p>Calculate Power</p>");
+// document.write("<input id='base' placeholder='Base value'>");
+// document.write("<input id='power' placeholder='Exponent value'>");
+// document.write("<p id='result'></p>");
+// document.write("<button type='submit' onclick='CalculatePower()'>Calculate</button>");
+// function CalculatePower() {
+//     var b = document.getElementById("base").value;
+//     var p = document.getElementById("power").value;
+//     var result;
+//     result = Power(b, p);
+//     document.getElementById("result").innerHTML = b + " power " +p+ " = " + result;
+// }
+document.write("<a href='#' onclick='Link()'> Please click this link </a>");
+function Link() {
+    alert("Thank you for clicking");
 }
-
 
 document.write("<h3>Question2</h3>");
 
