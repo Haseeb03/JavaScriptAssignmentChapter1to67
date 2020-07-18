7a5e03c3690c04d7c93adb0a2eca8081d9711cbf
Haseeb03 <m.haseeb5678@gmail.com>
2020-07-18 20:05:22 +0500

    JavaScript Assignment from Chapter1 to Chapter6

--- Chapter58 - 67/app.js
+++ Chapter58 - 67/app.js
@@ -1,16 +1,16 @@
 document.write("<h3>Question1</h3>");
-document.write("<p>Calculate Power</p>");
-document.write("<input id='base' placeholder='Base value'>");
-document.write("<input id='power' placeholder='Exponent value'>");
-document.write("<p id='result'></p>");
-document.write("<button type='submit' onclick='CalculatePower()'>Calculate</button>");
-function CalculatePower() {
-    var b = document.getElementById("base").value;
-    var p = document.getElementById("power").value;
-    var result;
-    result = Power(b, p);
-    document.getElementById("result").innerHTML = b + " power " +p+ " = " + result;
+document.write("<h3>(i)</h3>");
+var mc = document.getElementById("main-content");
+
+document.write("<h3>(ii)</h3>");
+// document.write("<br><br>All Child nodes are: "+mc.childNodes);
+
+document.write("<h3>(iii)</h3>");
+var rander = document.getElementsByClassName('render');
+for (let i = 0; i < rander.length; i++) {
+    document.write('<br><br>Render Class items: '+rander[i].innerHTML);
 }
+
 
 
 document.write("<h3>Question2</h3>");
