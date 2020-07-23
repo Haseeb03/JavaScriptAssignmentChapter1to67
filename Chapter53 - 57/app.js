4f84ae79a9891aece1138c37d79c7901e406e022
Haseeb03 <m.haseeb5678@gmail.com>
2020-07-23 16:11:17 +0500

    Saylani JavaScript Assignment from Chapter1 to Chapter67

--- Chapter53 - 57/app.js
+++ Chapter53 - 57/app.js
@@ -1,25 +1,18 @@
 document.write("<h3>Question1</h3>");
-document.write("<p>Calculate Power</p>");
-document.write("<input id='base' placeholder='Base value'>");
-document.write("<input id='power' placeholder='Exponent value'>");
-document.write("<p id='result'></p>");
-document.write("<button type='submit' onclick='CalculatePower()'>Calculate</button>");
-function CalculatePower() {
-    var b = document.getElementById("base").value;
-    var p = document.getElementById("power").value;
-    var result;
-    result = Power(b, p);
-    document.getElementById("result").innerHTML = b + " power " +p+ " = " + result;
+var modal = document.getElementById("myModal");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+  modal.style.display = "none";
 }
 
 
 document.write("<h3>Question2</h3>");
 
-
-document.write("<h3>Question3</h3>");
-
-
-document.write("<h3>Question4</h3>");
-
-
-document.write("<h3>Question5</h3>");
